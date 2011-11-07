ad6e43f2f1b93d150e51a80d74292277ceb1db12
Wayne Larsen <wayne@larsen.st>
2011-11-07 13:07:50 -0500

    Rename package: llog -> nanolog

--- test/run.js
+++ test/run.js
@@ -1,39 +1,39 @@
 
-var llog = require('../')
+var log = require('../')
 
-llog.info("You can use it straight out of the box");
+log.info("You can use it straight out of the box");
 
-llog.I("I Shortcut")
+log.I("I Shortcut")
 
-var warnlogger = llog.create({level: 'warn'})
+var warnlogger = log.create({level: 'warn'})
 warnlogger.info("This should not log")
 warnlogger.warn("This should log")
 
-llog.info("You can display objects as well", {msg: "Hello World"})
+log.info("You can display objects as well", {msg: "Hello World"})
 
-llog.to(llog.out.stderr())
-llog.info("This should now be sent to stderr and only stderr")
+log.to(log.out.stderr())
+log.info("This should now be sent to stderr and only stderr")
 
-dup = llog.dup({level: 'warn'})
+dup = log.dup({level: 'warn'})
 dup.warn("Dup should now send stuff to stderr")
 dup.info("But not info")
 
-llog.set({modules: {feature: 'debug', feature2: 'warn'}})
+log.set({modules: {feature: 'debug', feature2: 'warn'}})
 
-var feat = llog.module("feature")
+var feat = log.module("feature")
 feat.debug("feature debug: not blank")
 
-var feat2 = llog.module("feature2")
+var feat2 = log.module("feature2")
 feat2.info("feature2 info: blank")
 
-var feat3 = llog.module("feature3")
+var feat3 = log.module("feature3")
 feat3.info("module will behave as normal, If not overriden")
 feat3.debug("so this won't be shown")
 
-llog.info("this should not be blank")
+log.info("this should not be blank")
 
-var newlog = llog.create()
-newlog.use(llog.m.formatter("%timestamp% [%level%] %message%", 
+var newlog = log.create()
+newlog.use(log.m.formatter("%timestamp% [%level%] %message%", 
                             {timestamp: function(date) { return date.getFullYear(); }}))
 newlog.info("This should have a timestamp, formatted as full year")
 
